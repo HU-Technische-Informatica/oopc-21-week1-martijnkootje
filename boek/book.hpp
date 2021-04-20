c069f294f2bc05839c057b02cdf13ae5fa5653c5
martijnkootje <66223803+martijnkootje@users.noreply.github.com>
2021-04-20 14:34:42 +0200

    Add files via upload

---
+++ boek/book.hpp
@@ -0,0 +1,31 @@
+#ifndef BOOK_HPP
+#define BOOK_HPP
+#include <iostream>
+
+using namespace std;
+
+class book {
+private:
+
+string text;
+string author;
+string title;
+
+void print_text();
+void print_author();
+void print_title();
+
+public:
+
+book(
+const string & text,
+const string & author,
+const string & title
+):
+text( text ),
+author( author ),
+title( title )
+{}
+void print();
+};
+#endif // BOOK_HPP
---
+++ boek/main.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "book.hpp"
+
+int main(int argc, char **argv)
+{
+	book boek("h", "h", "h");
+	boek.print();
+	return 0;
+}
